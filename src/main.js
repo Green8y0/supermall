976f5721b676f18649ae526c31ba8b7e8a7adafc
Green8y0 <461197931@qq.com>
2021-04-06 16:52:14 +0800

    引入轮播图组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import router from './router'
 // import store from './store'
 
 // createApp(App).use(store).use(router).mount('#app')
-
 createApp(App).use(router).mount('#app')
 // createApp(App).mount('#app')
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -10,11 +10,25 @@
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import {getHomeMultidata} from 'network/home'
 
   export default {
     name: "Home",
     components: {
       NavBar
+    },
+    data() {
+      return {
+        banner: [],
+        recommend: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        console.log(res);
+        this.banner = res.data.banner.list
+        this.recommend = res.data.recommend.list
+      })
     }
   }
 </script>
