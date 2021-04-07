9ecf128bc256f32d9c350711a4f41e4e82885ceb
Green8y0 <461197931@qq.com>
2021-04-07 14:41:36 +0800

    添加商品列表组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,41 +1,157 @@
 <template>
   <div id="home">
+    <!-- 导航栏 -->
     <nav-bar class="home-nav">
       <template v-slot:center>
         <div>购物街</div>
       </template>
     </nav-bar>
+
+    <!-- 轮播图 -->
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view />
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
+
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li>24</li>
+      <li>25</li>
+      <li>26</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li>30</li>
+      <li>31</li>
+      <li>32</li>
+      <li>33</li>
+      <li>34</li>
+      <li>35</li>
+      <li>36</li>
+      <li>37</li>
+      <li>38</li>
+      <li>39</li>
+      <li>40</li>
+      <li>41</li>
+      <li>42</li>
+      <li>43</li>
+      <li>44</li>
+      <li>45</li>
+      <li>46</li>
+      <li>47</li>
+      <li>48</li>
+      <li>49</li>
+      <li>50</li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
+  import FeatureView from './childComps/FeatureView.vue'
+
   import NavBar from 'components/common/navbar/NavBar'
-  import {getHomeMultidata} from 'network/home'
+  import TabControl from 'components/content/tabControl/TabControl'
+  
+  import {
+    getHomeMultidata,
+    getHomeGoods
+  } from 'network/home'
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl
     },
     data() {
       return {
-        banner: [],
-        recommend: []
+        banners: [],
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        }
       }
     },
     created() {
-      getHomeMultidata().then(res => {
-        console.log(res);
-        this.banner = res.data.banner.list
-        this.recommend = res.data.recommend.list
-      })
+      // 1.请求轮播图与推荐数据
+      this.getHomeMultidata()
+
+      // 2.请求商品数据
+      this.getHomeGoods('pop')
+      // this.getHomeGoods('new')
+      // this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          console.log(res);
+          this.banners = res.data.banner.list
+          this.recommends = res.data.recommend.list
+        }).catch(err => {
+          console.log('从后端传回来的错误信息：', err);
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type, page).then(res => {
+          console.log("从后端传回来的信息 = ", res);
+          this.goods[type].list = this.goods[type].list.concat(res.data.list)
+          this.goods[type].page += 1
+        }).catch(err => {
+          console.log('从后端传回来的错误信息：', err);
+        })
+      }
     }
   }
 </script>
 
 <style lang="css" scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 3;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <swiper class="home-swiper">
+      <swiper-item v-for="item in banners" :key="item">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: 'HomeSwiper',
+    props: {
+      banners: Array,
+      dafault() {
+        return []
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+  .home-swiper img{
+    height: 20%;
+    width: 100%;
+  }
+</style>
